d583dfcd5e2dec3db76d799e35e75d2a299e14bd
JulianSalas2 <salasjulian544@gmail.com>
2024-12-15 19:22:05 -0500

    Corrigiendo detalles

--- User/forms.py
+++ User/forms.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from .models import Avatar
 
 class UserRegisterForm(UserCreationForm):
+    username = forms.CharField(label="Username")
     email = forms.EmailField()
     password1 = forms.CharField(label='Contraseña', widget=forms.PasswordInput)
     password2 = forms.CharField(label='Repetir contraseña', widget=forms.PasswordInput)
